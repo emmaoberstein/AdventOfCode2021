91c9778acb98bc843631ea91334db238b23e64ee
emmaoberstein <emmaoberstein@gmail.com>
2021-12-20 18:23:53 -0500

    Create part1.cs

---
+++ 17/part1.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{	
+	public static void Main()
+	{
+		var yTarget = -150;	
+		int v = -yTarget-1;
+		int y = 0;
+		while (v >= 0){
+			y += v--;
+		}
+			
+        Console.WriteLine(y);
+	}
+}
